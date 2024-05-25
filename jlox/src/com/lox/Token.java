d1127ef85c6974756aa4249ca458a6cc5d833d72
Angus Li <angusr.li02@gmail.com>
2024-05-24 20:36:36 -0400

    Documentation

--- jlox/src/com/lox/Token.java
+++ jlox/src/com/lox/Token.java
@@ -3,9 +3,17 @@ package jlox.src.com.lox;
 public class Token {
     public final TokenType type;
     public final String lexeme;
-    public final Object literal; 
-    public final int line; 
+    public final Object literal;
+    public final int line;
 
+    /**
+     * Implement lox tokens.
+     * 
+     * @param type    see TokenType
+     * @param lexeme  the raw substring of source code corresponding to the token
+     * @param literal the token's value if it is a literal; null otherwise
+     * @param line    the token's line in source code
+     */
     public Token(TokenType type, String lexeme, Object literal, int line) {
         this.type = type;
         this.lexeme = lexeme;
@@ -13,6 +21,9 @@ public class Token {
         this.line = line;
     }
 
+    /**
+     * Represent the token as a string.
+     */
     public String toString() {
         return type + " " + lexeme + " " + literal;
     }
